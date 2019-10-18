cbebe47deb658d732e76c36623b567399b9b7bd7
pootz10 <pootz10@gmail.com>
2019-10-17 22:41:33 -0300

    Update Linkfy_TL_NFO_Links.user.js
    update regex to detect when url has only www.

--- Linkfy_TL_NFO_Links.user.js
+++ Linkfy_TL_NFO_Links.user.js
@@ -3,8 +3,8 @@
 // @namespace   pootz10
 // @description NFO urls beginning with http become clickable
 // @include     /(^https?:\/\/www\.|^https?:\/\/)torrentleech\.org\/(torrent).*/
-// @version     1.0
-// @history     1.0 -
+// @version     1.1
+// @history     1.1 - update regex to detect when url has only www.
 // @license     GNU
 // @require     https://code.jquery.com/jquery-2.1.1.min.js
 // @updateURL   https://raw.githubusercontent.com/pootz10/Linkfy-TL-NFO-Links/master/Linkfy_TL_NFO_Links.user.js
@@ -13,14 +13,15 @@
 // @run-at      document-idle
 // ==/UserScript==
 
-
 $('#nfo_text').each( function() {
 
-    var re = /https?:\/\/(\S+)/g
+    //var re = /https?:\/\/(\S+)/g
+    var re = /\(?(?:https?:\/\/|www\.)(\S+)/g
     var link = '<a href="https://' + "$1" + '" style="color:yellow" target="_blank">https://' + "$1" + '</a>';
 
     var linkify = $(this).text().replace(re, link);
     linkify = linkify.replace(/https?:\/\/https?:\/\//g, "https://");
+    linkify = linkify.replace(/\/'|\/"/g, "/");
     $(this).html(linkify);
 
 });
