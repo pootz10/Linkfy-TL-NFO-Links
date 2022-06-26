53b2ee13eea8d0dc8b17d7fe50b64e2ab67ff1cd
pootz10 <pootz10@gmail.com>
2022-06-26 16:23:52 -0300

    Update Linkfy_TL_NFO_Links.user.js

--- Linkfy_TL_NFO_Links.user.js
+++ Linkfy_TL_NFO_Links.user.js
@@ -3,7 +3,8 @@
 // @namespace   pootz10
 // @description NFO urls beginning with http become clickable
 // @include     /(^https?:\/\/www\.|^https?:\/\/)torrentleech\.org\/(torrent).*/
-// @version     1.4
+// @version     1.5
+// @history     1.5 - Language added on highlight list
 // @history     1.4 - fix issue where script stopped working after firefox 84.x
 // @history     1.3 - highlight subtitles (não lançado na net)
 // @history     1.2 - minor fix, a missing space between ' " could not change color of the link on some browsers
@@ -21,7 +22,8 @@ $('#nfo_text').each( function() {
     var re = /\(?(?:https?:\/\/|www\.)(\S+)/g;
     var link = '<a href="https://' + "$1" + ' " style="color:yellow;" target="_blank">https://' + "$1" + '</a>';
 
-    var reSUBs    = /(Subtitles|Subtitles.+?$|Subs[\W].+?$|SUBTITLES:?$)/mg;
+    //var reSUBs    = /(Subtitles|Subtitles.+?$|Subs[\W].+?$|SUBTITLES:?$|Language\s+:?\s?[A-Za-z]+$)/mg;
+    var reSUBs    = /(Subs[\W].+?$|SUBTITLES:?$|Language\s+:?\s?[A-Za-z, ]+$|Subtitles?:?(\s|\.)+[A-Za-z, ]+$|Subtitles.+?$|Subtitles)/gm;
     var subs      = '<font' + ' style="color:lime;" >' + "$1" + '<font style="color:white;">';
 
     var linkify = $(this).text().replace(re, link).replace(reSUBs, subs);
